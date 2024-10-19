762e86014c11a50bbfab1a58d13cd1e19678060c
Sean Dolan <sean.f.dolan@gmail.com>
2024-10-19 14:54:06 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -7,5 +7,10 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-bootstrap/Button']
+    }
   }
 })
